d89375d4bdc47c087a0207441ba0d8e2f92cc8e8
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-11-19 11:24:04 +1100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,149 @@
+#[derive(Default, Clone)]
+struct Task {
+    id: u32,
+    text: String,
+    done: bool,
+}
+
+#[derive(Default)]
+struct TodoList {
+    tasks: Vec<Task>,
+    next_id: u32,
+}
+
+impl TodoList {
+    fn add(&mut self, text: String) -> &Task {
+        let id = if self.tasks.is_empty() {
+            1
+        } else {
+            self.next_id
+        };
+        self.tasks.push(Task {
+            id,
+            text,
+            done: false,
+        });
+        self.next_id = id + 1;
+        self.tasks.last().unwrap()
+    }
+
+    fn print_list(&self) {
+        for task in &self.tasks {
+            println!("{}: {} -- Done: {}", task.id, task.text, task.done);
+        }
+    }
+
+    fn mark_done(&mut self, id: u32) -> Result<&Task, TodoError> {
+        match self.tasks.get_mut(id as usize) {
+            Some(t) => {
+                t.done = true;
+                Ok(t)
+            }
+            None => Err(TodoError::TaskNotFound),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Command {
+    Add { text: String },
+    List,
+    Done { id: u32 },
+    Help,
+}
+
+#[derive(Debug)]
+enum TodoError {
+    CommandError,
+    TaskNotFound,
+}
+
+fn main() -> Result<(), TodoError> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let cmd: Command = parse_command(args)?;
+    let mut task_list: TodoList = Default::default();
+    run(cmd, &mut task_list)?;
+    // println!("{:?}", cmd);
+    Ok(())
+}
+
+fn run(cmd: Command, todo_list: &mut TodoList) -> Result<(), TodoError> {
+    match cmd {
+        Command::Add { text } => {
+            todo_list.add(text);
+            Ok(())
+        }
+        Command::List => {
+            println!("Printing tasks...");
+            todo_list.print_list();
+            Ok(())
+        }
+        Command::Done { id } => {
+            println!("Marking {id} as done...");
+            match todo_list.mark_done(id) {
+                Ok(_) => println!("Task id {} marked as done!", id),
+                Err(_) => println!("Task id {} not found", id),
+            }
+            todo!("command 'done' not implemented");
+        }
+        Command::Help => {
+            println!("Available commands: Add, List, Help");
+            Ok(())
+        }
+    }
+}
+
+fn parse_command(args: Vec<String>) -> Result<Command, TodoError> {
+    let mut args_iter = args.into_iter();
+    let sub = args_iter.next().ok_or(TodoError::CommandError)?;
+
+    match sub.as_str() {
+        "add" => {
+            let text = args_iter.next().ok_or(TodoError::CommandError)?;
+            Ok(Command::Add { text })
+        }
+        "done" => {
+            let id = args_iter.next().ok_or(TodoError::CommandError)?;
+            let id: u32 = id.parse().unwrap();
+
+            Ok(Command::Done { id })
+        }
+        "list" => Ok(Command::List),
+        "help" => Ok(Command::Help),
+        _ => Err(TodoError::CommandError),
+    }
+}
+
+#[test]
+fn test_cmd_parsing() -> Result<(), TodoError> {
+    let args: Vec<String> = vec![String::from("add"), String::from("hello there")];
+    let expected = Command::Add {
+        text: "hello there".to_string(),
+    };
+    assert_eq!(expected, parse_command(args)?);
+    Ok(())
+}
+
+#[test]
+fn test_add() -> Result<(), TodoError> {
+    let args1: Vec<String> = vec![String::from("add"), String::from("hello there")];
+    let args2: Vec<String> = vec![String::from("add"), String::from("hello there")];
+    let cmd1 = parse_command(args1)?;
+    let cmd2 = parse_command(args2)?;
+    let mut task_list: TodoList = Default::default();
+    run(cmd1, &mut task_list)?;
+    run(cmd2, &mut task_list)?;
+    assert_eq!(task_list.tasks.len(), 2);
+    Ok(())
+}
+
+#[test]
+fn test_mark_done() -> Result<(), TodoError> {
+    let args: Vec<String> = vec![String::from("add"), String::from("hello there")];
+    let cmd = parse_command(args)?;
+    let mut task_list: TodoList = Default::default();
+    run(cmd, &mut task_list)?;
+    let res = task_list.mark_done(0)?;
+    assert!(res.done);
+    Ok(())
+}
